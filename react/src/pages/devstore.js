97c221798b0a1114affd0c2fe9c0bab86906fc86
KaiqueYudji <yudjikaique@gmail.com>
2021-10-06 11:20:29 -0300

    ok

--- react/src/pages/devstore.js
+++ react/src/pages/devstore.js
@@ -246,7 +246,7 @@ export default function Devjs(){
 
                     {produtos.map((item, i )=>
                     
-                        <tr className={i %2 == 0 ?"linha-alternada" :""} >
+                        <tr className={i %2 === 0 ?"linha-alternada" :""} >
                             <td> <img style={{height:'3em', width:'3em'}} src={item.img_produto} alt="img-produto"/> </td>
                             <td> {item.id_produto} </td>
                             <td title={item.nm_produto.length >= 25 ?item.nm_produto :''}> { item.nm_produto != null && item.nm_produto.length >=25 ?item.nm_produto.substr(0,25) + '...' :item.nm_produto } </td>
